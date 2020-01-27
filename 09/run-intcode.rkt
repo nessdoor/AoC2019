9bfe035a662b0e8753c4d772ec8066dbf7352ec4
Tomas Antonio Lopez <tomasantonio.lopez@mail.polimi.it>
2020-01-27 22:03:28 +0100

    Removed no longer used vector parameter

--- 09/run-intcode.rkt
+++ 09/run-intcode.rkt
@@ -29,7 +29,7 @@
     (vector-set! memory-vect addr value)))
 
 (define resolver ; decode the content of a parameter based on the m access mode
-  (lambda (v i m)
+  (lambda (i m)
     (case m
         ((0) (memr (memr i)))                      ; position
         ((1) (memr i))                             ; immediate
@@ -42,40 +42,40 @@
       (memw (+ relative-base (memr index)) value))))    ; relative
 
 (define alu ; execute an arithmetic op
-  (lambda (v op ip m0 m1 m2)
+  (lambda (op ip m0 m1 m2)
     (write-at (+ 3 ip) m2
               ((case op
                  ((1) +)
                  ((2) *)
                  (else 'undefined))
-               (resolver v (+ 1 ip) m0)
-               (resolver v (+ 2 ip) m1)))
+               (resolver (+ 1 ip) m0)
+               (resolver (+ 2 ip) m1)))
     (+ 4 ip)))
 
 (define io ; execute an I/O instruction
-  (lambda (v op ip m input output)
+  (lambda (op ip m input output)
     (case op
       ((3) (display "> ")
            (when (not (or (char-ready? input) (eq? 'nocont exit-cont))) ; if the reading procedure would block, try and call the exit-cont
              (set! exit-cont (call/cc (lambda (cc) (exit-cont cc)))))
            (write-at (+ 1 ip) m (string->number (read-line input))))
-      ((4) (displayln (resolver v (+ 1 ip) m) output))
+      ((4) (displayln (resolver (+ 1 ip) m) output))
       (else (error "Unexpected IO opcode: " op)))
     (+ 2 ip)))
 
 (define branch ; execute a branch instruction
-  (lambda (v op ip m0 m1)
-    (let ([param (resolver v (+ 1 ip) m0)])
+  (lambda (op ip m0 m1)
+    (let ([param (resolver (+ 1 ip) m0)])
       (if (case op
             ((5) (not (zero? param)))
             ((6) (zero? param)))
-          (resolver v (+ 2 ip) m1)
+          (resolver (+ 2 ip) m1)
           (+ 3 ip)))))
 
 (define compare ; execute a comparison instruction
-  (lambda (v op ip m0 m1 m2)
-    (let ([p0 (resolver v (+ 1 ip) m0)]
-          [p1 (resolver v (+ 2 ip) m1)])
+  (lambda (op ip m0 m1 m2)
+    (let ([p0 (resolver (+ 1 ip) m0)]
+          [p1 (resolver (+ 2 ip) m1)])
       (write-at (+ 3 ip) m2
                 (if (case op
                       ((7) (< p0 p1))
@@ -85,12 +85,12 @@
     (+ ip 4)))
 
 (define rebase ; move the relative base
-  (lambda (v ip m0)
-    (set! relative-base (+ relative-base (resolver v (+ ip 1) m0)))
+  (lambda (ip m0)
+    (set! relative-base (+ relative-base (resolver (+ ip 1) m0)))
     (+ ip 2)))
 
 (define run-intcode ; run an intcode program
-  (lambda (v input-port output-port (cc 'nocont))
+  (lambda (program input-port output-port (cc 'nocont))
     (define ric-rec
       (lambda (ip)
         (let* ([instruction (memr ip)]
@@ -100,14 +100,14 @@
                [mode2 (decoder instruction 4)])
           (unless (= 99 opcode) ; stop at opcode 99
             (ric-rec (case opcode
-                            ((1 2) (alu v opcode ip mode0 mode1 mode2))             ; arithmetic ops branch
-                            ((3 4) (io v opcode ip mode0 input-port output-port))   ; I/O branch
-                            ((5 6) (branch v opcode ip mode0 mode1))                ; jumps branch
-                            ((7 8) (compare v opcode ip mode0 mode1 mode2))         ; comparison branch
-                            ((9) (rebase v ip mode0))                               ; adjust relative base
+                            ((1 2) (alu opcode ip mode0 mode1 mode2))             ; arithmetic ops branch
+                            ((3 4) (io opcode ip mode0 input-port output-port))   ; I/O branch
+                            ((5 6) (branch opcode ip mode0 mode1))                ; jumps branch
+                            ((7 8) (compare opcode ip mode0 mode1 mode2))         ; comparison branch
+                            ((9) (rebase ip mode0))                               ; adjust relative base
                             (else (error "Unexpected opcode: " opcode))))))))
 
-    (set! memory-vect v)
+    (set! memory-vect program)
     (set! exit-cont cc)
     (ric-rec 0)
     (when (not (eq? 'nocont exit-cont)) ; if exit-cont is set, call it as soon as the Intcode program stops
