7e8c02eb1fc5090277aee69a056a900d4ba3ef20
Tomas Antonio Lopez <tomasantonio.lopez@mail.polimi.it>
2020-02-02 21:21:37 +0100

    Remodeled computer state

--- 02/IntcodeInterpreter.hs
+++ 02/IntcodeInterpreter.hs
@@ -4,8 +4,7 @@ import qualified Data.IntMap.Strict as IMS
 
 type Intcode = IMS.IntMap Int
 type Opcode = Int
-data CompState = CompState {instrPtr :: Int, memory :: Intcode}
-type StopFlag = Bool
+data CompState = CompState {instrPtr :: Int, memory :: Intcode, halted :: Bool}
 
 loadProgram :: String -> Intcode
 loadProgram s = IMS.fromList $ zip [0..] $ map read $ words . map replComma $ s
@@ -14,22 +13,23 @@ loadProgram s = IMS.fromList $ zip [0..] $ map read $ words . map replComma $ s
 
 retrieveOperand = IMS.findWithDefault 0
 
-elemOp :: Opcode -> CompState -> CompState
-elemOp opcode state@(CompState ip mem) = CompState (ip + 4) $ IMS.insert (destination state) ((case opcode of
-                                                                                                 1 -> (+)
-                                                                                                 2 -> (*)) (operand1 state) (operand2 state)) mem
-    where operand1 (CompState ip mem) = retrieveOperand (retrieveOperand (ip + 1) mem) mem
-          operand2 (CompState ip mem) = retrieveOperand (retrieveOperand (ip + 2) mem) mem
-          destination (CompState ip mem) = retrieveOperand (ip + 3) mem
+elemOp :: Opcode -> (Int, Intcode) -> (Int, Intcode)
+elemOp opcode state@(ip, mem) = ((ip + 4), IMS.insert (destination state) ((case opcode of
+                                                                              1 -> (+)
+                                                                              2 -> (*)) (operand1 state) (operand2 state)) mem)
+    where operand1 (ip, mem) = retrieveOperand (retrieveOperand (ip + 1) mem) mem
+          operand2 (ip, mem) = retrieveOperand (retrieveOperand (ip + 2) mem) mem
+          destination (ip, mem) = retrieveOperand (ip + 3) mem
 
-execStep :: CompState -> (CompState, StopFlag)
-execStep state@(CompState ip mem) = let operation = IMS.lookup ip mem
-                                     in case operation of
-                                          Nothing -> error "Out of bounds"
-                                          Just 99 -> (state, True)
-                                          Just opcode -> (elemOp opcode state, False)
+execStep :: CompState -> CompState
+execStep (CompState ip mem h) = let operation = IMS.lookup ip mem
+                                 in case operation of
+                                      Nothing -> error "Out of bounds"
+                                      Just 99 -> CompState ip mem True
+                                      Just opcode -> let (newIp, newMem) = elemOp opcode (ip, mem)
+                                                      in CompState newIp newMem False
 
 runIntcode :: Intcode -> Intcode
-runIntcode program = runner (CompState 0 program, False)
-    where runner (st, True) = memory st
-          runner (st, False) = runner $ execStep st
+runIntcode program = runner $ CompState 0 program False
+    where runner (CompState _ mem True) = mem
+          runner state = runner $ execStep state
